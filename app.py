f9171411b3d8648bc2f4d563888fad71fc084453
James Neild <jamesneild45@gmail.com>
2022-07-11 17:23:49 +0100

    added more bootstrap features + images

--- app.py
+++ app.py
@@ -4,18 +4,6 @@ from views import my_view
 app = Flask(__name__)
 app.register_blueprint(my_view)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host="0.0.0.0")
